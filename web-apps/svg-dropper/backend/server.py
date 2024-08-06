35568ac8ba571d8ce3f820016e56fc83006cfad0
tristanryerparke <57774376+tristanryerparke@users.noreply.github.com>
2024-08-06 14:29:05 +0200

    added http://sofia-plotter.local:8081 to cors origins

--- web-apps/svg-dropper/backend/server.py
+++ web-apps/svg-dropper/backend/server.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8081"],
+    allow_origins=["http://localhost:8081", "http://sofia-plotter.local:8081"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
