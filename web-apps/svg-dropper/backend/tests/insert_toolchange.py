5b106d2bf4f85ad55086aa570e420ab5d500b74c
tristanryerparke <57774376+tristanryerparke@users.noreply.github.com>
2025-06-11 18:10:57 -0400

    test run

--- web-apps/svg-dropper/backend/tests/insert_toolchange.py
+++ web-apps/svg-dropper/backend/tests/insert_toolchange.py
@@ -10,11 +10,13 @@ def insert_tool_commands(file_path, N):
         if line.startswith('G1 Z') and '20.00' in line:
             tool_counter += 1
             if tool_counter % N == 0:
+                # lines.insert(i + 1, tool_command + '\n' + 'M98 P"/macros/dip_in_paint.g"\n')
                 lines.insert(i + 1, tool_command + '\n')
                 tool_command = 'T2' if tool_command == 'T1' else 'T1'
+                
 
     with open(file_path, 'w') as file:
         file.writelines(lines)
 
 # Usage
-insert_tool_commands('/Users/tristanryerparke/Downloads/vangoh_minimal_two_tool_800.gcode', 3)
+insert_tool_commands('/Users/tristanryerparke/Downloads/maya_vg_two_brush_raw.gcode', 5)
